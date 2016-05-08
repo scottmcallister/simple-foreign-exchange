3cb1757b1b310712c1f7c76810c91095c544d408
Scott McAllister <scottm@nextopia.com>
2016-05-08 14:37:26 -0400

    Added Volley to project and made a request to the exchange rate API

--- app/src/main/java/mrscottmcallister/com/simpleforeignexchange/MainActivity.java
+++ app/src/main/java/mrscottmcallister/com/simpleforeignexchange/MainActivity.java
@@ -1,13 +1,48 @@
 package mrscottmcallister.com.simpleforeignexchange;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String url = "http://api.fixer.io/latest?base=usd";
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        text = (TextView) findViewById(R.id.hello_text);
+        JsonObjectRequest getExchangeRates = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>(){
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        text.setText("Response: " + response.toString());
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+                        System.out.println("error...");
+                    }
+                });
+        queue.add(getExchangeRates);
     }
 }
