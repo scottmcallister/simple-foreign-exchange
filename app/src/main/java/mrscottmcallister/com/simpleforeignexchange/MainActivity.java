8e007612caa0ef24b9f12a2bc1db43688882faf9
Scott McAllister <scottm@nextopia.com>
2016-05-11 10:05:27 -0400

    use different API to get exchange rates for more countries/currencies

--- app/src/main/java/mrscottmcallister/com/simpleforeignexchange/MainActivity.java
+++ app/src/main/java/mrscottmcallister/com/simpleforeignexchange/MainActivity.java
@@ -5,21 +5,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONObject;
-
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String url = "http://api.fixer.io/latest?base=";
-    private String base = "usd";
+    private String url = "https://currency-exchange.p.mashape.com/exchange";
+    private String from = "USD";
+    private String to = "CAD";
+    private String apiKey = "PLum3weAI4mshDYIvKhIyYvSuNSHp1EzY7fjsneJdFaHScp6zQ";
     private TextView text;
     private RequestQueue queue;
 
@@ -31,23 +34,30 @@ public class MainActivity extends AppCompatActivity {
         // get JSON data from API
         queue = Volley.newRequestQueue(this);
         text = (TextView) findViewById(R.id.hello_text);
-        JsonObjectRequest getExchangeRates = new JsonObjectRequest(
+        StringRequest getExchangeRates = new StringRequest(
                 Request.Method.GET,
-                url + base,
-                null,
-                new Response.Listener<JSONObject>(){
+                url + "?from=" + from + "&val=1.0&to=" + to,
+                new Response.Listener<String>(){
                     @Override
-                    public void onResponse(JSONObject response) {
-                        text.setText("Response: " + response.toString());
+                    public void onResponse(String response) {
+                        text.setText("Response: " + response);
                     }
                 },
                 new Response.ErrorListener(){
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         // TODO Auto-generated method stub
-                        System.out.println("error...");
+                        text.setText("Error: " + error.getMessage());
                     }
-                });
+                }){
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    HashMap<String, String> headers = new HashMap<String, String>();
+                    headers.put("X-Mashape-Key", apiKey);
+                    headers.put("Accept", "text/plain");
+                    return headers;
+                }
+        };
         queue.add(getExchangeRates);
 
         // Initialize DB with country info
